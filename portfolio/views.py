4a25ca2ed75073830b2b69dc9a39b6d29b058f87
sps <suryaprakash0923@gmail.com>
2025-06-21 19:59:58 +0530

    first commit

---
+++ portfolio/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request,'website/index.html')
+
+def about(request):
+    return HttpResponse("this is about page ")
