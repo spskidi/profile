7712680f7dc6a4c1340f47c9f23cda9382e947fd
sps <suryaprakash0923@gmail.com>
2025-06-21 20:41:03 +0530

    deployss

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+set -e
+
 echo "build start"
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
+
+# Collect static files into the directory Vercel expects
+python3 manage.py collectstatic --noinput --clear -i admin -i node_modules --settings=profileportfolio.settings
+
 echo "build end"
