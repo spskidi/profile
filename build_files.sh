a4f375d34f8ba345e4b7f2a75147c064eb421812
sps <suryaprakash0923@gmail.com>
2025-06-22 00:24:55 +0530

    Rename requirements file for Vercel

--- build_files.sh
+++ build_files.sh
@@ -2,9 +2,9 @@
 set -e
 
 echo "build start"
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
 
-python3 manage.py collectstatic --noinput --clear -i admin -i node_modules --settings=portfolio.settings
+python manage.py collectstatic --noinput --clear -i admin -i node_modules
 
 echo "build end"
