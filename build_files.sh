95c0aead306f1874d5ea090756b883bb84d1fefe
sps <suryaprakash0923@gmail.com>
2025-06-22 12:01:53 +0530

    hl

--- build_files.sh
+++ build_files.sh
@@ -5,6 +5,6 @@ echo "build start"
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 
-python3 manage.py collectstatic --noinput --clear -i admin -i node_modules
+# python3 manage.py collectstatic --noinput --clear -i admin -i node_modules
 
 echo "build end"
